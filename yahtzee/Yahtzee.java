1a7da2654d0ee51f2cee6d468cc303c9cf03c760
pontushenriksson <pontushenrikssn@gmail.com>
2022-10-11 14:44:09 +0200

    upload

---
+++ yahtzee/Yahtzee.java
@@ -0,0 +1,15 @@
+package yahtzee;
+
+import java.util.Scanner;
+
+public class Yahtzee {
+
+    public static void main(String[] args) {
+        // Dependencies
+        Scanner input = new Scanner(System.in);
+        
+        // Variables
+        
+        // Code
+    }
+}
