e8a38ff66b1bc5819ee8ee602730a682c0e1c137
pontushenriksson <pontushenrikssn@gmail.com>
2022-10-17 16:20:22 +0200

    moved readme err: currentArray prints every step!

---
+++ devlog.md
@@ -0,0 +1,21 @@
+> Oct 11, 2022
+
+- First commit with all of the dependencies and no other code.
+
+> Oct 14, 2022
+
+- Moved the read.me file to the root folder, fixed spelling errors in the read.me file, removed some unneeded dependencies, made the ones combination array, put all of the current dices in an array and added re-roll availability.
+  **Errors:**
+
+  - **The program only works 20% of the time, it receives an "Index 6 out of bounds for length 6" error**
+
+> Oct 17, 2022
+
+- Fixed the old "Index 6 out of bounds for length 6" error and the program will now work 100% of the time by removing the +1 by the randomizer and changed the maount of dices from 6 to the actual 5 dices, added the devlog.md, moved the devlog.md to the root directory.
+  **Errors:**
+
+  - **~~The program only works 20% of the time, it receives an "Index 6 out of bounds for length 6" error~~**
+
+  - **~~Can't print the currentArray outside the loop~~**
+
+  - **Prints every step of the currentArray**
--- yahtzee/Yahtzee.java
+++ yahtzee/Yahtzee.java
@@ -18,7 +18,9 @@ public class Yahtzee {
         Scanner Input = new Scanner(System.in);
         int[][] tärningar = { { 6, 5, 4, 3, 2, 1 }, { 6, 5, 4, 3, 2, 1 }, { 6, 5, 4, 3, 2, 1 }, { 6, 5, 4, 3, 2, 1 },
                 { 6, 5, 4, 3, 2, 1 } };
+
         ArrayList<Integer> combOne = new ArrayList<Integer>();
+
         combOne.add(1);
         combOne.add(11);
         combOne.add(111);
@@ -30,6 +32,8 @@ public class Yahtzee {
         int sorted3 = 0;
         int sorted4 = 0;
         int sorted5 = 0;
+        ArrayList<Integer> currentArray = new ArrayList<Integer>();
+
         int roll = 0;
         int dice1Roll = 1;
         int dice2Roll = 1;
@@ -39,6 +43,7 @@ public class Yahtzee {
 
         int firstTime = 1;
         int printAll = 1;
+        int ggr = 0;
 
         // Code
         System.out.println("Welcome!" + " " + "Type 1 to start and type 2 to see the rules!");
@@ -47,7 +52,6 @@ public class Yahtzee {
         while (i == 1) {
             if (firstTime == 1) {
                 System.out.println("####################");
-
                 System.out.println("Starting in 3 seconds...");
                 Thread.sleep(1000);
                 System.out.println("2 seconds...");
@@ -59,7 +63,6 @@ public class Yahtzee {
                 firstTime--;
             }
             System.out.println("----------------");
-
             System.out.println("Rolling dices...");
 
             System.out.println("----------------");
@@ -69,7 +72,7 @@ public class Yahtzee {
                 int TärningsSlag1 = (int) (Math.random() * 5);
                 int NummerPåTärning1 = (int) (Math.random() * 5);
                 int a = Integer.valueOf((tärningar[TärningsSlag1][NummerPåTärning1]));
-                ArrayList<Integer> currentArray = new ArrayList<Integer>();
+
                 if (dice1Roll == 1 && i == 1 || sorted1 == 0) {
                     sorted1 = a;
 
@@ -92,6 +95,7 @@ public class Yahtzee {
                 currentArray.add(sorted3);
                 currentArray.add(sorted4);
                 currentArray.add(sorted5);
+
                 if (printAll == 5) {
                     System.out.println(currentArray);
                 }
@@ -99,6 +103,7 @@ public class Yahtzee {
                 i++;
 
             }
+
             i = 0;
             System.out.println("Do you want to re-roll any dice? 1 = yes, 0 = no.");
             roll = Input.nextInt();
@@ -109,35 +114,44 @@ public class Yahtzee {
                 System.out.println("Do you want to re-roll dice one with a" + " " + sorted1 + "?" + " "
                         + "1 for yes and 0 for no.");
                 dice1Roll = Input.nextInt();
-                System.out.println("Do you want to re-roll" + " " + sorted2 + "?" + " " + "1 for yes and 0 for no.");
+                System.out
+                        .println("Do you want to re-roll" + " " + sorted2 + "?" + " " + "1 for yes and 0 for no.");
                 dice2Roll = Input.nextInt();
-                System.out.println("Do you want to re-roll" + " " + sorted3 + "?" + " " + "1 for yes and 0 for no.");
+                System.out
+                        .println("Do you want to re-roll" + " " + sorted3 + "?" + " " + "1 for yes and 0 for no.");
                 dice3Roll = Input.nextInt();
-                System.out.println("Do you want to re-roll" + " " + sorted4 + "?" + " " + "1 for yes and 0 for no.");
+                System.out
+                        .println("Do you want to re-roll" + " " + sorted4 + "?" + " " + "1 for yes and 0 for no.");
                 dice4Roll = Input.nextInt();
-                System.out.println("Do you want to re-roll" + " " + sorted5 + "?" + " " + "1 for yes and 0 for no.");
+                System.out
+                        .println("Do you want to re-roll" + " " + sorted5 + "?" + " " + "1 for yes and 0 for no.");
                 dice5Roll = Input.nextInt();
-                // System.out.println(currentArray);
 
             } else if (roll != 1 || roll != 0) {
                 System.out.println("Please enter 1 or 0. Enter 1 to re-roll and 0 to skip.");
             } else if (roll == 0) {
-                System.out.println("Skipping re-roll!");
+
+            }
+            ggr++;
+
+            if (ggr == 3) {
+                i = 0;
+            }
+        }
+        if (ggr != 3) {
+            if (i == 2) {
+                System.out.println("####################");
+                System.out.println(
+
+                        "- Gameplay: In each turn a player may throw the dices up to three times. A player doesn't have to roll all five dices on the second and third throw of a round, he may put as many dice as he wants to the side and only throw the ones that don't have the numbers he's trying to get. For example, a player throws and gets 1,3,3,4,6. He decides he want to try for the large straight, 1,2,3,4,5. So, he puts 1,3,4 to the side and only throws 3 and 6 again, hoping to get 2 and 5.");
+                System.out.println("----------------");
+                System.out.println(
+                        "- Combinations: Hehe");
+                System.out.println("####################");
+            } else if (i != 1) {
+                System.out.println("Wrong Input, Reload game and then enter the number 1 in the terminal");
             }
 
         }
-        if (i == 2) {
-            System.out.println("####################");
-            System.out.println(
-
-                    "- Gameplay: In each turn a player may throw the dices up to three times. A player doesn't have to roll all five dices on the second and third throw of a round, he may put as many dice as he wants to the side and only throw the ones that don't have the numbers he's trying to get. For example, a player throws and gets 1,3,3,4,6. He decides he want to try for the large straight, 1,2,3,4,5. So, he puts 1,3,4 to the side and only throws 3 and 6 again, hoping to get 2 and 5.");
-            System.out.println("----------------");
-            System.out.println(
-                    "- Combinations: Hehe");
-            System.out.println("####################");
-        } else if (i != 1) {
-            System.out.println("Wrong Input, Reload game and then enter the number 1 in the terminal");
-        }
-
     }
 }
